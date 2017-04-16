458ee6bc558b6d01dfad346cb80870e1f5cd4ced
Chakra Bhandari <chakrabhandari.pp@gmail.com>
2017-04-17 04:33:55 +1000

    my first commit

---
+++ src/main/java/com/rd/domain/ServiceError.java
@@ -0,0 +1,22 @@
+package com.rd.domain;
+
+/**
+ * @author Roland Krüger
+ */
+public class ServiceError {
+    private String error;
+    private String message;
+
+    public ServiceError(Exception exception) {
+        this.message = exception.getMessage();
+        this.error = exception.getClass().getSimpleName();
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

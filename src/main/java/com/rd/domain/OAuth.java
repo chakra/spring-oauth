c6f39391adae0f7b59cd3df68a828ffae19f3bea
Chakra Bhandari <chakrabhandari.pp@gmail.com>
2017-05-07 19:04:46 +1000

    add swagger

---
+++ src/main/java/com/rd/domain/OAuth.java
@@ -0,0 +1,15 @@
+package com.rd.domain;
+
+/**
+ * Created by chakrabhandari on 7/05/2017.
+ */
+public class OAuth {
+
+    private String username;
+    private String password;
+    private String grantType;
+    private String client_id;
+    private String client_secret;
+
+
+}
